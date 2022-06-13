823f5cec37cf268e11f45489c5f4022128adb635
Helder Pinhal <helder@notifica.re>
2022-06-13 09:34:17 +0100

    refactor: update asset parsing

---
+++ Notificare Go/Network/APIClient+Payloads.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient+Payloads.swift
+//  Notificare Go
+//
+//  Created by Helder Pinhal on 09/06/2022.
+//
+
+import Foundation
--- Notificare Go/UI/Views/ProductsList/ProductsListViewModel.swift
+++ Notificare Go/UI/Views/ProductsList/ProductsListViewModel.swift
@@ -20,10 +20,25 @@ class ProductsListViewModel: ObservableObject {
         Task {
             do {
                 let assets = try await Notificare.shared.assets().fetch(group: "products")
-                let productList = assets.first!.extra["products"]
-                
-                let encoded = try JSONEncoder().encode(AnyCodable(productList))
-                products = try JSONDecoder().decode([Product].self, from: encoded)
+                self.products = assets.compactMap { (asset) -> Product? in
+                    guard let id = asset.extra["id"] as? String,
+                          let description = asset.description,
+                          // let price = asset.extra["price"] as? Double,
+                          let imageUrl = asset.url,
+                          let highlighted = asset.extra["highlighted"] as? Bool
+                    else { return nil }
+                    
+                    let price: Double
+                    if let parsed = asset.extra["price"] as? Int {
+                        price = Double(parsed)
+                    } else if let parsed = asset.extra["price"] as? Double {
+                        price = parsed
+                    } else {
+                        return nil
+                    }
+                    
+                    return Product(id: id, name: asset.title, description: description, price: price, imageUrl: imageUrl, highlighted: highlighted)
+                }
             } catch {
                 print(error.localizedDescription)
             }
