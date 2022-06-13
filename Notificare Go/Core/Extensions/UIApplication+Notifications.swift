95ed2c9cadadb9ec768a8dc5c78083bcd422d37b
Helder Pinhal <helder@notifica.re>
2022-06-13 09:48:40 +0100

    refactor: simpler map

--- Notificare Go/Core/Extensions/UIApplication+Notifications.swift
+++ Notificare Go/Core/Extensions/UIApplication+Notifications.swift
@@ -14,8 +14,7 @@ extension UIApplication {
     var currentKeyWindow: UIWindow? {
         UIApplication.shared.connectedScenes
             //.filter { $0.activationState == .foregroundActive }
-            .map { $0 as? UIWindowScene }
-            .compactMap { $0 }
+            .compactMap { $0 as? UIWindowScene }
             .first?.windows
             .filter { $0.isKeyWindow }
             .first
